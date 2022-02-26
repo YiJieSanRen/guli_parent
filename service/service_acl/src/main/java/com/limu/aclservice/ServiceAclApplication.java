3f680ce6a2eae47a4ae70c3e3fe2af7f73e9bca4
limu <limu@qq.com>
2022-02-26 10:07:06 +0800

    第十四次提交-使用实现后台登录以及权限管理

---
+++ service/service_acl/src/main/java/com/limu/aclservice/ServiceAclApplication.java
@@ -0,0 +1,17 @@
+package com.limu.aclservice;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan("com.limu")
+@MapperScan("com.limu.aclservice.mapper")
+public class ServiceAclApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAclApplication.class,args);
+    }
+}
